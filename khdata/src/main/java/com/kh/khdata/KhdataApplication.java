e784d1f1f2a83cbd2ebfd84cb103f569aa6c5422
hiphop5782 <zdlsz@hanmail.net>
2025-10-01 10:04:43 +0900

    최초 프로젝트 배포

---
+++ khdata/src/main/java/com/kh/khdata/KhdataApplication.java
@@ -0,0 +1,13 @@
+package com.kh.khdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhdataApplication.class, args);
+	}
+
+}
---
+++ khdata/src/test/java/com/kh/khdata/KhdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.khdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
