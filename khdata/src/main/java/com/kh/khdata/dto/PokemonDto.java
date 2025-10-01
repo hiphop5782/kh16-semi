b60b7d1f8cd494ec95f68d57f54164ea6e06df63
jaeyeah <limjh819@naver.com>
2025-10-01 10:36:56 +0900

    PokemonDto 내용추가

--- khdata/src/main/java/com/kh/khdata/dto/PokemonDto.java
+++ khdata/src/main/java/com/kh/khdata/dto/PokemonDto.java
@@ -1,5 +1,12 @@
 package com.kh.khdata.dto;
 
+import lombok.Data;
+
+@Data
 public class PokemonDto {
 	//백승연
+	
+	private int pokemonNo;
+	private String pokemonName;
+	private String pokemonType;
 }
