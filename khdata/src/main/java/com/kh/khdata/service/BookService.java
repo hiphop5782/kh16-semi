85916839c4b4b00cd29febfb782069ff24e2da98
SadMental <sadrapper99@gmail.com>
2025-10-01 10:38:38 +0900

    BookService 수정

--- khdata/src/main/java/com/kh/khdata/service/BookService.java
+++ khdata/src/main/java/com/kh/khdata/service/BookService.java
@@ -1,6 +1,5 @@
 package com.kh.khdata.service;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +8,7 @@ public class BookService {
 	
 	// 이위섭.
 	@Transactional
-	public void error(String book_name) {
+	public int error(String book_name) {
+		return -1;
 	}
 }
